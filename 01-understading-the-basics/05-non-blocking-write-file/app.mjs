d6ccccbb9a7e0b26a9a01c1b1e22000b1cddc3aa
ch <mkmsacc@outlook.com>
2021-06-17 08:47:08 +0700

    Remove write file sync

--- 01-understading-the-basics/05-non-blocking-write-file/app.mjs
+++ 01-understading-the-basics/05-non-blocking-write-file/app.mjs
@@ -28,8 +28,7 @@ const server = http.createServer((req, res) => {
             const parsedBody = Buffer.concat(body).toString();
             console.log(parsedBody);
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-            fs.writeFile('message.txt', message, err => {
+            fs.writeFile('message.txt', message, () => {
                 res.statusCode = 302;
                 res.setHeader('Location', '/');
                 return res.end();
