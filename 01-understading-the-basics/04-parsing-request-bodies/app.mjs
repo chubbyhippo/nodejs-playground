3fda2e2ff9501a076e0f6b60f0068ed585c7eacb
ch <mkmsacc@outlook.com>
2021-06-17 08:21:49 +0700

    Add return

--- 01-understading-the-basics/04-parsing-request-bodies/app.mjs
+++ 01-understading-the-basics/04-parsing-request-bodies/app.mjs
@@ -24,7 +24,7 @@ const server = http.createServer((req, res) => {
             console.log(chunk);
             body.push(chunk);
         });
-        req.on('end', () => {
+        return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             console.log(parsedBody);
             const message = parsedBody.split('=')[1];
