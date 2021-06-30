894d7713fed30aac8404bf8f47fde2cbb9e6636a
ch <mkmsacc@outlook.com>
2021-06-30 07:45:05 +0700

    Finish sharing data across requests and users

--- 04-working-with-dynamic-content-and-adding-template-engines/01-sharing-data-across-requests-and-users/routes/shop.js
+++ 04-working-with-dynamic-content-and-adding-template-engines/01-sharing-data-across-requests-and-users/routes/shop.js
@@ -2,9 +2,12 @@ const express = require('express');
 const path = require('path');
 
 const router = express.Router();
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    console.log('shop.js', adminData.products);
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
