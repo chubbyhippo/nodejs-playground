395e903cd90019afe28ed6f802eca94477b9a9cb
ch <mkmsacc@outlook.com>
2023-11-19 20:47:38 +0700

    Clean all

--- 01-understading-the-basics/00-creating-a-node-server/app.mjs
+++
@@ -1,8 +0,0 @@
-import http from 'http';
-// const http = require('http')
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    // process.exit()
-});
-
-server.listen(3000);
--- 02-improved-development-workflow/01-npm/app.mjs
+++
@@ -1,8 +0,0 @@
-import http from 'http';
-
-import * as routes from './routes.mjs';
-
-const server = http.createServer(routes.requestHandler);
-console.log(routes.test);
-
-server.listen(3000);
